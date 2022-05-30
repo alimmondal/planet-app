16cf5bd46029d87ef6404f22f7a1837f9368404d
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-05-30 22:01:25 +0600

    updated

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
-import { View, StyleSheet, FlatList, Pressable } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, FlatList, Pressable, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import Text from '../components/text/text';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import PlanetHeader from '../components/planet-header';
@@ -7,36 +7,60 @@ import { colors } from '../themes/colors';
 import { PLANET_LIST } from '../data/planet-list';
 import { spacing } from '../themes/spacing';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+const PlanetItem = ({ item }) => {
+  const { name, color } = item;
+  const navigation = useNavigation();
+  return (
+    <Pressable
+      onPress={() => {
+        navigation.navigate('Details', { planet: item });
+      }}
+      style={styles.items}
+    >
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+        <View style={[styles.circle, { backgroundColor: color }]} />
+        <Text preset="h4" style={styles.itemName}>
+          {name}
+        </Text>
+      </View>
+      <AntDesign name="right" size={18} color="white" />
+    </Pressable>
+  );
+};
 
 export default function Home({ navigation }) {
+  const [list, setList] = useState(PLANET_LIST);
+  const renderItem = ({ item }) => {
+    return <PlanetItem item={item} />;
+  };
+
+  const searchFilter = (text) => {
+    const filteredList = PLANET_LIST.filter((item) => {
+      const itemName = item.name.toLocaleLowerCase();
+      const userTypeText = text.toLocaleLowerCase();
+      return itemName.indexOf(userTypeText) > -1;
+    });
+    setList(filteredList);
+    console.log('Filtered', filteredList);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <TextInput
+        placeholder="Type your planet name"
+        placeholderTextColor={colors.white}
+        autoCorrect={false}
+        style={styles.searchInput}
+        onChangeText={(text) => searchFilter(text)}
+      />
       <FlatList
         contentContainerStyle={styles.list}
-        data={PLANET_LIST}
+        data={list}
         keyExtractor={(item) => item.name}
-        renderItem={({ item }) => {
-          const { name, color } = item;
-          return (
-            <Pressable
-              onPress={() => {
-                navigation.navigate('Details', { planet: item });
-              }}
-              style={styles.items}
-            >
-              <View
-                style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}
-              >
-                <View style={[styles.circle, { backgroundColor: color }]} />
-                <Text preset="h4" style={styles.itemName}>
-                  {name}
-                </Text>
-              </View>
-              <AntDesign name="right" size={18} color="white" />
-            </Pressable>
-          );
-        }}
+        renderItem={renderItem}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
     </SafeAreaView>
@@ -69,5 +93,12 @@ const styles = StyleSheet.create({
   separator: {
     borderBottomColor: colors.white,
     borderWidth: 0.5,
+  },
+  searchInput: {
+    padding: spacing[4],
+    color: colors.white,
+    borderBottomColor: colors.white,
+    borderBottomWidth: 1,
+    margin: spacing[5],
   },
 });
